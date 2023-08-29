c53184fb05ff429d4db5debdc17a6524997562f4
gabreudev <biel0alves0@gmail.com>
2023-08-29 07:11:31 -0300

    feat: initial commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <title>UseSalome</title>
+</head>
+<body>    
+  <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo center">UseSalome</a>
+      <ul id="" class="right">
+        <li><a href="">Home</a></li>
+        <li><a href="">Carrinho</a></li>
+        <li><a href="">Sobre</a></li>
+      </ul>
+    </div>
+  </nav>
+
+  @yield('conteudo')
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+            
+</body>
+</html>
---
+++ resources/views/sobre.blade.php
@@ -0,0 +1,16 @@
+@extends('layout') <!-- Certifique-se de que seu layout padrão esteja configurado em resources/views/layouts/app.blade.php -->
+
+@section('conteudo')
+
+<div class="container">
+    <div class="row">
+        <div class="col s12">
+            <h2 class="header">Sobre</h2>
+            <p>O site UseSalome foi criado para divulgar os produtos da influencer Salomé. </p> 
+            <p>Os produtos eram divulgados apenas em sua rede social, este site veio para proporcionar aos clientes mais informações sobre os produtos sem a necessidade de um contato direto com a Salomé. </p>
+            <p>Aplicação mvc Laravel 8 by GabreuDev</p>
+        </div>
+    </div>
+</div>
+
+@endsection
